f2e18ec4fabfc86815a503d84d366dd18d160930
Maxwilliam Gomes dos Santos <117930231+MaxwilliamGomes@users.noreply.github.com>
2023-09-23 07:02:20 -0300

    Update and rename code.py to Calculadora_pedidos.py

--- Calculadora_pedidos.py
+++ Calculadora_pedidos.py
@@ -1,3 +1,14 @@
+Calculadora de pagamentos
+A ideia aqui é criar uma calculadora de pagamentos compartilhada.
+
+Sabe aquele momento em que estamos numa mesa com os amigos e temos que pagar a conta, mas você só pediu uma lata de Coca-Cola?
+
+Isso mesmo, essa calculadora vai te ajudar a pagar apenas pelo que consumiu com o percentual da gorjeta referente ao que consumiu.
+
+
+
+
+
 print("Welcome to tip calculator.")
 
 value = float(input("Was a total bill? $ "))
